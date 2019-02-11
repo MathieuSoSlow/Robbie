c67b09e900ba8394ec2a0d64b94d9d530c4c9832
MathieuSoSlow <MathieuSoSlow@users.noreply.github.com>
2019-02-11 18:50:53 +0100

    [Unity] Started battle manager

---
+++ Robbie/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    private List<Enemy> enemy;
+    private Player player;
+
+    [SerializeField] private float tempo;
+    [SerializeField] private float timer;
+
+    [SerializeField] private float halfWindow;
+    [SerializeField] private float perfectHalfWindow;
+
+    [SerializeField] private bool canAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.fixedDeltaTime;
+
+        if (timer > tempo)
+        {
+            canAttack = true;
+            timer = 0;
+        }
+    }
+
+    public void AttackEnemy(KeyCode ak)
+    {
+        var currentTime = timer;
+        if (!canAttack)
+            return;
+
+        var halfTempo = tempo / 2;
+        var lowerBound = halfTempo - halfWindow;
+        var higherBound = halfTempo + halfWindow;
+
+        var perfectLowerBound = halfTempo - perfectHalfWindow;
+        var perfectHigherBound = halfTempo + perfectHalfWindow;
+
+        if (currentTime > perfectLowerBound && currentTime < perfectHigherBound)
+        {
+
+        }
+
+        if (currentTime > lowerBound && currentTime > higherBound)
+        {
+            canAttack = false;
+        }
+    }
+
+    public void AttackPlayer(float damage)
+    {
+
+    }
+
+}
--- Robbie/Assets/Scripts/Enemy.cs
+++ Robbie/Assets/Scripts/Enemy.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private List<KeyCode> lifePoints;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,24 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    bool Defend(KeyCode kc)
+    {
+        if (lifePoints.Count == 0)
+            return true;
+        if (lifePoints.First() == kc)
+        {
+            lifePoints.RemoveAt(0);
+            return true;
+        }
+
+        return false;
+    }
+
+    void RemoveLastLifePoint()
+    {
+        if (lifePoints.Count > 0)
+            lifePoints.RemoveAt(lifePoints.Capacity - 1);
     }
 }
